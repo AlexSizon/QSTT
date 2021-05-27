10bf0246906f605e61d0f2dd58b900b5262feb2b
AlexSizon <37938267+AlexSizon@users.noreply.github.com>
2021-05-27 08:01:33 +0300

    Initial commit

---
+++ Assets/Scripts/PlateRoataion.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlateRoataion : MonoBehaviour
+{
+    public static bool isGameStarted = false;
+    public Transform PlateTrransform;
+    public Transform SphereTransform;
+    public GameObject GameOverMenu;
+    public Text StopWatchText;
+    float horizontalSpeed = 2.0f;
+    float verticalSpeed = 2.0f;
+    float startTime = 0;
+    void Update()
+    {
+        if (isGameStarted)
+        {
+            StopWatch(true);
+        }
+        if (!(SphereTransform.position.y < -3f))
+        {
+            float h = horizontalSpeed * Input.GetAxis("Horizontal") * -1;
+            float v = verticalSpeed * Input.GetAxis("Vertical");
+            PlateTrransform.Rotate(v, 0, h);
+        }
+        else
+        {
+            isGameStarted = false;
+            StopWatch(false);
+            GameOverMenu.SetActive(true);
+        }
+    }
+    public void StopWatch(bool IsWorking)
+    {
+        if (IsWorking)
+        {
+            startTime += Time.deltaTime;
+            StopWatchText.text = startTime.ToString("F2");
+        }
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UIManger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManger : MonoBehaviour
+{
+    public void StartGame(Rigidbody SphereBody)
+    {
+        SphereBody.useGravity = true;
+        PlateRoataion.isGameStarted = true;
+    }
+    public void DisActivePanel(GameObject Panel)
+    {
+        Panel.SetActive(false);
+    }
+    public void RestatrtGame(Rigidbody SphereBody)
+    {
+        SceneManager.LoadScene(0);
+        StartGame(SphereBody);
+    }
+}
